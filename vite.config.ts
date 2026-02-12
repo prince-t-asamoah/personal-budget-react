c432c5d41068170ee7caca0b71794454d2abb344
Prince Asamoah <prince.t.asamoah@gmail.com>
2026-02-12 10:57:53 -1200

    fix: remove server config to fix build issue

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
- server: {
-    port: 8000
-     host: true,
-    strictPort: true,
-  },
   plugins: [
     react({
       babel: {
