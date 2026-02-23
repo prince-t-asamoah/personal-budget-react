e00876edd9066f17455fe21cace3495c8606c8c1
Prince Asamoah <prince.t.asamoah@gmail.com>
2026-02-23 21:58:15 +0000

    refactor: remove envelopes povider

--- src/pages/Budget/components/BudgetRoot.tsx
+++ src/pages/Budget/components/BudgetRoot.tsx
@@ -1,24 +1,19 @@
 import { useState, useEffect } from "react";
 import AppLoader from "../../../components/AppLoader/AppLoader";
-import BudgetStateProvider from "../../../providers/BudgetStateProvider";
 import BudgetPage from "../BudgetPage";
 
 export default function BudgetRoot() {
-    const [isLoading, setIsLoading] = useState(true);
-      useEffect(() => {
-        const timer = setTimeout(() => {
-          setIsLoading(false);
-        }, 3000);
-        return () => clearTimeout(timer);
-      }, []);
-    
-      if (isLoading) {
-        return <AppLoader />;
-      }
-      
-  return (
-    <BudgetStateProvider>
-      <BudgetPage />
-    </BudgetStateProvider>
-  );
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (isLoading) {
+    return <AppLoader />;
+  }
+
+  return <BudgetPage />;
 }
