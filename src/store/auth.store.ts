aad78d297f338ca23372886083a1c906fc616d7f
Prince Asamoah <prince.t.asamoah@gmail.com>
2026-02-23 22:38:52 +0000

    feat: create auth state and reducers

--- src/store/auth.store.ts
+++ src/store/auth.store.ts
@@ -5,8 +5,20 @@ export const authState: AuthState = {
     isAuthenticated: false,
 };
 
-export const authReducer = (state: AuthState, actions: AuthStateActions) => {
-    switch (actions.type) {
+export const authReducer = (state: AuthState, action: AuthStateActions) => {
+    switch (action.type) {
+        case 'SET_IS_AUTHENTICATED': {
+            return {
+                ...state,
+                isAuthenticated: action.payload
+            } as AuthState
+        }
+        case 'SET_USER': {
+            return {
+                ...state,
+                user: action.payload
+            } as AuthState
+        }
         default:
             return state;
     }
