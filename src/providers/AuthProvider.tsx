fac7bff5fb0e305aba9dd33dca5decd456767549
Prince Asamoah <prince.t.asamoah@gmail.com>
2026-02-25 01:54:46 +0000

    feat: use persistent reducer for context

--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -1,14 +1,21 @@
-import { useReducer, type ReactNode } from "react";
+import { type ReactNode } from "react";
 import { AuthContext } from "../context/auth.context";
 import { authReducer, authState } from "../store/auth.store";
+import type { AuthState, AuthStateActions } from "../models/auth.model";
+import usePersistentReducer from "../hooks/usePersistentReducer";
 
 type AuthProviderProviderProps = {
   children: ReactNode;
 };
 
 export default function AuthProvider({ children }: AuthProviderProviderProps) {
-  const [state, dispatch] = useReducer(authReducer, authState);
-
+  
+const [state, dispatch] = usePersistentReducer<AuthState, AuthStateActions>({
+  key: 'auth',
+  reducer: authReducer,
+  initialState: authState
+})
+;
   return (
     <AuthContext.Provider value={{ state, dispatch }}>
       {children}
