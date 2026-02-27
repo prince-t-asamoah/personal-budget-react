aa4fc029ed8314e12a37ee19f34b85803950247c
Prince Asamoah <prince.t.asamoah@gmail.com>
2026-02-27 16:39:48 +0000

    refactor: update services to use body headers constant

--- src/services/apis/envelopesApi.service.ts
+++ src/services/apis/envelopesApi.service.ts
@@ -1,36 +1,61 @@
 import type { Envelope } from "../../models/envelopes.model";
 import httpClientService from "../httpClient.service";
 
-const BASE_ROUTE = '/envelopes';
+const BASE_ROUTE = "/envelopes";
+const BODY_HEADERS = {
+    "Content-Type": "application/json",
+}
 
-export const fetchEnvelopes = () => httpClientService(BASE_ROUTE, { method: 'GET' });
+const envelopesApiService = (url: string, options: RequestInit) =>
+    httpClientService(`${BASE_ROUTE}${url}`, { ...options, credentials: "include" });
 
-export const createEnvelope = (envelope: Pick<Envelope, 'name' | 'allocatedAmount' | 'currency' | 'spentAmount'>) => httpClientService(BASE_ROUTE, {
-    method: 'POST', body: JSON.stringify(envelope), headers: {
-        'Content-Type': 'application/json'
-    }
-});
+export const fetchEnvelopes = () => envelopesApiService('/', { method: 'GET' });
 
-export const transferEnvelopeFunds = ({ fromId, toId, amount }: { fromId: string, toId: string, amount: number }) => httpClientService(`${BASE_ROUTE}/transfer/${fromId}/${toId}`, {
-    method: 'POST', body: JSON.stringify({ amount }), headers: {
-        'Content-Type': 'application/json'
-    }
-});
+export const createEnvelope = (
+    envelope: Pick<
+        Envelope,
+        "name" | "allocatedAmount" | "currency" | "spentAmount"
+    >,
+) =>
+    envelopesApiService('/', {
+        method: "POST",
+        body: JSON.stringify(envelope),
+        headers: BODY_HEADERS
+    });
 
-export const updateEnvelopeFunds = (id: string, data: Partial<Envelope>) => httpClientService(`${BASE_ROUTE}/${id}`, {
-    method: 'PATCH',
-    body: JSON.stringify(data), headers: {
-        'Content-Type': 'application/json'
-    }
-});
+export const transferEnvelopeFunds = ({
+    fromId,
+    toId,
+    amount,
+}: {
+    fromId: string;
+    toId: string;
+    amount: number;
+}) =>
+    envelopesApiService(`/transfer/${fromId}/${toId}`, {
+        method: "POST",
+        body: JSON.stringify({ amount }),
+        headers: BODY_HEADERS
+    });
 
-export const deleteEnvelope = (id: string) => httpClientService(`${BASE_ROUTE}/${id}`, {
-    method: 'DELETE',
-});
+export const updateEnvelopeFunds = (id: string, data: Partial<Envelope>) =>
+    envelopesApiService(`/${id}`, {
+        method: "PATCH",
+        body: JSON.stringify(data),
+        headers: BODY_HEADERS
+    });
 
-export const distributeFunds = (data: { amount: number, envelopesId: string[] }) => httpClientService(`${BASE_ROUTE}/distribute`, {
-    method: 'POST',
-    body: JSON.stringify(data), headers: {
-        'Content-Type': 'application/json'
-    }
-})
+export const deleteEnvelope = (id: string) =>
+    envelopesApiService(`/${id}`, {
+        method: "DELETE",
+    });
+
+export const distributeFunds = (data: {
+    amount: number;
+    envelopesId: string[];
+}) =>
+    envelopesApiService('/distribute', {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: BODY_HEADERS
+    });
