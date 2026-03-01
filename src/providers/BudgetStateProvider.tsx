4d42c044d44785454292e7c79df4367f97b7d250
Prince Asamoah <prince.t.asamoah@gmail.com>
2026-03-01 13:47:59 +0000

    feat: persistn envelopes state

--- src/providers/BudgetStateProvider.tsx
+++ src/providers/BudgetStateProvider.tsx
@@ -1,17 +1,28 @@
-import { type ReactNode, useReducer } from "react";
+import { type ReactNode } from "react";
 import { envelopeReducer, envelopeState } from "../store/budget-envelope.store";
 
 import { BudgetContext } from "../context/budget.context";
+import usePersistentReducer from "../hooks/usePersistentReducer";
+import {
+  type EnvelopeState,
+  type EnvelopeStateActions,
+} from "../models/envelopes.model";
 
 type BudgetStateProviderProps = {
   children: ReactNode;
 };
 
-export default function BudgetStateProvider({ children }: BudgetStateProviderProps) {
-  const [state, dispatch] = useReducer(
-    envelopeReducer,
-    envelopeState,
-  );
+export default function BudgetStateProvider({
+  children,
+}: BudgetStateProviderProps) {
+  const [state, dispatch] = usePersistentReducer<
+    EnvelopeState,
+    EnvelopeStateActions
+  >({
+    key: "envelopes",
+    reducer: envelopeReducer,
+    initialState: envelopeState,
+  });
 
   return (
     <BudgetContext.Provider value={{ state, dispatch }}>
