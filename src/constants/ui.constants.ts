6dae3ef16095d39132e703358b419dbaefd11c8b
Prince Asamoah <prince.t.asamoah@gmail.com>
2026-03-05 17:49:43 +0000

    feat: create form currency data for select

--- src/constants/ui.constants.ts
+++ src/constants/ui.constants.ts
@@ -1,2 +1,17 @@
 export const NAVIGATION_TIMEOUT = 2000;
 export const TOAST_NOTIFICATION_DURATION = 5000;
+export const FORM_CURRENCY_DATA = [
+    { name: "GHS (Ghanaian Cedi)", value: "GHS" },
+    {
+        name: "USD (US Dollar)",
+        value: "USD",
+    },
+    {
+        name: "EUR (Euro)",
+        value: "EUR",
+    },
+    {
+        name: "GBP (British Pound)",
+        value: "GBP",
+    },
+];
