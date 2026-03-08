899de7dc855ff06490384ab9128d4e96253cddbe
Prince Asamoah <prince.t.asamoah@gmail.com>
2026-03-08 02:31:37 +0000

    feat: create transaction types

---
+++ src/models/transactions.model.ts
@@ -0,0 +1,33 @@
+
+
+export const TransactionType = {
+    INITIAL_ALLOCATION: 'initial_allocation',
+    FUNDING: 'funding',
+    EXPENSE: 'expense',
+    TRANSFER_IN: 'transfer_in',
+    TRANSFER_OUT: 'transfer_out',
+    DISTRIBUTION: 'distribution',
+    ADJUSTMENT: 'adjustment'
+} as const;
+
+export type TransactionType = typeof TransactionType[keyof typeof TransactionType];
+
+export type Transaction = {
+    id: string;
+    type: TransactionType
+    amount: number;
+    currrency: string;
+    balance: number;
+    description: string;
+    notes: string;
+    referenceId: string;
+    createdAt: string;
+};
+
+export type TransactionsState = {
+    transactions: Transaction[]
+    isLoading: boolean;
+}
+
+export type TransactionsActions = { type: 'SET_TRANSACTIONS', payload: Transaction[] } |
+{ type: 'SET_IS_LOADING', payload: boolean }
