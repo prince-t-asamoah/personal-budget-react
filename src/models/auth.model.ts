ff82ef8fbf57f3d40d034907aba909bdf12ddc9a
Prince Asamoah <prince.t.asamoah@gmail.com>
2026-03-18 18:15:17 +0000

    feat: update store for is authenticating user

--- src/models/auth.model.ts
+++ src/models/auth.model.ts
@@ -26,8 +26,12 @@ export type AuthUser = {
 export type AuthState = {
   user: AuthUser | null;
   isAuthenticated: boolean;
+  isAuthenticating: boolean;
+  hasAuthenticationError: boolean;
 }
 
-export type AuthStateActions = 
-{ type: 'SET_IS_AUTHENTICATED', payload: boolean } |
-{ type: 'SET_USER', payload: AuthUser | null };
+export type AuthStateActions =
+  { type: 'SET_IS_AUTHENTICATED', payload: boolean } |
+  { type: 'SET_USER', payload: AuthUser | null } |
+  { type: 'SET_IS_AUTHENTICATING', payload: boolean } |
+  { type: 'SET_HAS_AUTHENTICATED_ERROR', payload: boolean };
--- src/store/auth.store.ts
+++ src/store/auth.store.ts
@@ -3,21 +3,35 @@ import type { AuthState, AuthStateActions } from "../models/auth.model";
 export const authState: AuthState = {
     user: null,
     isAuthenticated: false,
+    isAuthenticating: false,
+    hasAuthenticationError: false
 };
 
-export const authReducer = (state: AuthState, action: AuthStateActions) => {
+export const authReducer = (state: AuthState, action: AuthStateActions): AuthState => {
     switch (action.type) {
         case 'SET_IS_AUTHENTICATED': {
             return {
                 ...state,
                 isAuthenticated: action.payload
-            } as AuthState
+            }
         }
         case 'SET_USER': {
             return {
                 ...state,
                 user: action.payload
-            } as AuthState
+            }
+        }
+        case 'SET_IS_AUTHENTICATING': {
+            return {
+                ...state,
+                isAuthenticating: action.payload
+            }
+        }
+        case 'SET_HAS_AUTHENTICATED_ERROR': {
+            return {
+                ...state,
+                hasAuthenticationError: action.payload
+            }
         }
         default:
             return state;
