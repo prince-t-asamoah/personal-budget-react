9aaeff62cbd58c719b92c16a79eb8358fd405b4e
Prince Asamoah <prince.t.asamoah@gmail.com>
2026-03-18 18:16:48 +0000

    feat: show loader when authenticating user

--- src/guard/AuthGuard.tsx
+++ src/guard/AuthGuard.tsx
@@ -2,6 +2,7 @@ import { type ReactNode } from "react";
 import { Navigate } from "react-router-dom";
 import { APP_ROUTES } from "../constants/routes.constants";
 import { useAuthContext } from "../context/auth.context";
+import AppLoader from "../components/AppLoader/AppLoader";
 
 export default function AuthGuard({
   children,
@@ -9,6 +10,10 @@ export default function AuthGuard({
   children: ReactNode;
 }>) {
   const { state } = useAuthContext();
+
+  if (state.isAuthenticating) {
+    return <AppLoader message="Verifying session..." />;
+  }
 
   return (
     <>
